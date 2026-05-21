93b6aa9129ca7ab0c8ee8c4b776b1fc48a836053
01msmr <u@msmr.co>
2026-05-22 00:18:45 +0200

    tmp: extended htpasswd search

--- dirinfo.php
+++ dirinfo.php
@@ -3,9 +3,13 @@ $base = '/var/www/vhosts/hosting189417.ae8d9.netcup.net/a.msmr.co';
 $candidates = [
     $base . '/.htpasswd',
     $base . '/httpdocs/.htpasswd',
+    $base . '/httpdocs/edit/.htpasswd',
     $base . '/conf/.htpasswd',
+    $base . '/conf/htpasswd',
     dirname($base) . '/.htpasswd',
+    '/etc/httpd/.htpasswd',
+    '/usr/local/psa/admin/.htpasswd',
 ];
 foreach ($candidates as $f) {
-    echo (file_exists($f) ? '✓ FOUND' : '✗ nicht da') . ': ' . $f . '<br>';
+    echo (file_exists($f) ? '<b style="color:green">✓ FOUND</b>' : '✗') . ': ' . $f . '<br>';
 }
