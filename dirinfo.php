ff9b1070729eb623a5e0c991165f189fc8c886f9
01msmr <u@msmr.co>
2026-05-22 00:08:31 +0200

    fix: correct AuthUserFile path, add setup helper

--- dirinfo.php
+++
@@ -1,1 +0,0 @@
-<?php echo dirname($_SERVER['DOCUMENT_ROOT']); ?>
---
+++ setup.php
@@ -0,0 +1,26 @@
+<?php
+$htpasswd = '/var/www/vhosts/hosting189417.ae8d9.netcup.net/a.msmr.co/.htpasswd';
+$msg = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $user = trim($_POST['user'] ?? '');
+    $pass = $_POST['pass'] ?? '';
+    if ($user && $pass) {
+        $hash = password_hash($pass, PASSWORD_BCRYPT);
+        if (file_put_contents($htpasswd, "$user:$hash\n") !== false) {
+            $msg = '<p style="color:green">✓ .htpasswd erstellt. Diese Datei jetzt löschen!</p>';
+        } else {
+            $msg = '<p style="color:red">Fehler: Datei konnte nicht geschrieben werden.</p>';
+        }
+    }
+}
+?><!DOCTYPE html>
+<html><head><meta charset="utf-8"><title>Setup</title></head><body>
+<h3>htpasswd einrichten</h3>
+<?= $msg ?>
+<form method="post">
+  <input name="user" placeholder="Benutzername" required><br><br>
+  <input name="pass" type="password" placeholder="Passwort" required><br><br>
+  <button type="submit">Erstellen</button>
+</form>
+</body></html>
